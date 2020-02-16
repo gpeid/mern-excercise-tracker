75f257945cec327ba83afffd3be902e254b2a1ea
George <ghdiep@gmail.com>
2020-02-16 18:10:56 -0500

    add user and exercise endpoints

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,15 @@ connection.once('open', () => {
 	console.log('MongoDB database connection established successfully');
 });
 
+// Server API Endpoints
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
 });
+
